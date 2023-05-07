a2c5e1573d91a19e588eb0d5ddfc995f86dddaef
sanek <alexandr.panteleev2000@gmail.com>
2023-05-07 23:16:08 +0300

    fixed

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from sqlalchemy import URL
 
 DATABASE = URL.create(
     "postgresql+psycopg2",
-    username="db_username",
-    password="db_password",
+    username="sanek",
+    password="panteleev",
     host="localhost",
     database="ptmk",
     port='5432',
